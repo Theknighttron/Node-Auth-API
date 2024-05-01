9164ee32ed3b36f3302e1494486c621d9acd889a
polyhistor2050 <chrisspaul2050@gmail.com>
2024-05-01 23:24:06 +0300

    Function to update student record

--- app/controllers/user.contoller.js
+++ app/controllers/user.contoller.js
@@ -4,17 +4,17 @@ const Candidate = db.candidate;
 
 // for public access
 exports.allAccess = (req, res) => {
-  res.status(200).send("Public Content.");
+    res.status(200).send("Public Content.");
 };
 
 // for loggedin user (user/admin) role
 exports.userBoard = (req, res) => {
-  res.status(200).send("User Content.");
+    res.status(200).send("User Content.");
 };
 
 // for admin role
 exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
+    res.status(200).send("Admin Content.");
 };
 
 // get list of all student from the database
@@ -37,8 +37,6 @@ exports.getCandidates = async (req, res) => {
 exports.getCandidateByStudentId = async (req, res) => {
     const studentId = req.params.student_id;
 
-    console.log(studentId);
-
     try {
         // Find the candidate with the provided student_id
         const candidate = await Candidate.findOne({
@@ -55,3 +53,34 @@ exports.getCandidateByStudentId = async (req, res) => {
         res.status(500).send({  message: error.message });
     }
 };
+
+
+exports.updateStudent = async (req, res) => {
+    const studentId = req.params.student_id;
+
+    try {
+        // Find student by student id
+        const student = await Candidate.findByPk(studentId);
+
+        // If student not found reture 404
+        if(!student) {
+            return res.status(400).json({ message: "Student not found" });
+        }
+
+        // Update student details with data from request body
+        student.firstname = req.body.firstname;
+        student.lastname = req.body.lastname;
+        student.gender = req.body.gender;
+        student.email = req.body.email;
+        student.course = req.body.course;
+        student.level = req.body.level;
+
+        // Save the updated student record
+        await student.save();
+
+        // Send a success response
+        res.status(200).json({ message: "Student details updated successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
